d30a36de51ce0897147179f6e9433fbd072e04f2
Sarvar <103188744+Sarvar2004@users.noreply.github.com>
2022-08-05 19:56:04 +0500

    done

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,50 @@
 const input = document.querySelector(".contain");
 const questions = document.querySelector("#questions")
-const input1 = document.querySelector('.input')
-
+const savol = document.querySelector(".savol");
 const question = [
     {
-        question: "Afrikada nechta aholi bor",
+        question: "1_Afrikada nechta aholi bor",
         answer: "4000"
     },
     {
-        question: "Afrikada nechta aholi bor",
+        question: "2_Afrikada nechta aholi bor",
         answer: "4000"
     },
     {
-        question: "Afrikada nechta aholi bor",
+        question: "3_Afrikada nechta aholi bor",
         answer: "4000"
     },
     {
-        question: "Afrikada nechta aholi bor",
+        question: "4_Afrikada nechta aholi bor",
         answer: "4000"
-    }   
+    },
+    {
+        question: "5_Afrikada nechta aholi bor",
+        answer: "4000"
+    },
+    {
+        question: "6_Afrikada nechta aholi bor",
+        answer: "4000"
+    },
+    {
+        question: "7_Afrikada nechta aholi bor",
+        answer: "4000"
+    },
+    {
+        question: "8_Afrikada nechta aholi bor",
+        answer: "4000"
+    },
+    {
+        question: "9_Afrikada nechta aholi bor",
+        answer: "4000"
+    }    
 ]
 
 function inputHendle(){
     input.classList.add("none");
     questions.classList.remove("none")
+    savol.textContent = ""  
 }
-
-function myFunc(input,submit){
-
-}
-myFunc(input1,submit)
-
-
 
 
 
